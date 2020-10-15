c2e5ad4ed7e86e0b6687a27744e2cb98f9907cc3
kryfin <usman@Apples-MacBook-Pro.local>
2020-10-15 13:11:43 +0500

    new addition

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { map } from 'lodash';
 import template from './numberlist.hbs';
 
-
+//add
 
 let numbers = map([1,2,3,4,5,6], n=>n*n);
 document.getElementById('app-container').innerHTML = template({numbers});
