f19f068b64244ce53008c9286eb4f9eb66e67b29
kryfin <usman.m.razzaq@gmail.com>
2020-12-31 15:46:48 +0500

    fix changes

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     setupFiles: ['<rootDir>/src/test/setup.js'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-          '<rootDir>/test/jest/__mocks__/fileMock.js',
+          '<rootDir>/__mocks__/fileMock.js',
         '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
       },
 }
